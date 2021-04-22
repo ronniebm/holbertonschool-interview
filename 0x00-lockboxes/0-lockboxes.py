94d1660371985336c394f185267e9847fb8b2cbb
Ronnie <ronnie.coding@gmail.com>
2021-04-22 07:03:30 -0500

    testing exercise

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+"""
+A method that determines if all the boxes can be opened.
+Exercise: 0-lockboxes.py
+"""
+
+def canUnlockAll(boxes):
+    num_of_boxes = len(boxes)
+    list_of_keys = boxes[0]
+    box_locked = [False] + [True] * (num_of_boxes - 1)
+
+    for key in list_of_keys:
+        if ((key < num_of_boxes) and (box_locked[key] is True)):
+            box_locked[key] = False
+            list_of_keys.extend(boxes[key])
+
+    return not any(box_locked)
