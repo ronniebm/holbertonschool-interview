5cafaa1c1ce9a0aebc3a5c2ccaead3faaab046c0
ronniebm <ronnie.coding@gmail.com>
2022-01-23 18:59:18 -0500

    trying another way

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -1,145 +1,89 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
 /**
- * copy - copies data from one buffer to another
- *
- * @src: source buffer
- * @dst: destination buffer
- * @size: size of buffers
- *
- * Return: No Return
+ * merge_sub - merge sorting algorithm function.
+ *@subarray: array split into sub array.
+ *@buffer: buffer of array.
+ *@lo: low partition.
+ *@mid: mid partition.
+ *@hi: high partition.
+ * Return: Always 0
  */
-void copy(int *src, int *dst, int size)
+void merge_sub(int *subarray, int *buffer, size_t lo, size_t mid, size_t hi)
 {
-	int i;
+	size_t low;
+	size_t midi;
+	size_t a = 0;
 
-	for (i = 0; i < size; i++)
-		dst[i] = src[i];
-}
-/**
- * merge - merges two sets of data in ascending order
- * but they must already be sorted before hand
- * @array: first set of data
- * @buff: second set of data
- * @minL: lower range of first set of data
- * @maxL: upper range of first set of data
- * @minR: lower range of second set of data
- * @maxR: upper range of second set of data
- *
- * Return: No Return
- */
-void merge(int *array, int *buff, int minL, int maxL, int minR, int maxR)
-{
-	int i = minL, j = minR, k = minL;
-
-	while (i <= maxL || j <= maxR)
-
-		if (i <= maxL && j <= maxR)
-			if (buff[i] <= buff[j])
-				array[k] = buff[i], k++, i++;
-			else
-				array[k] = buff[j], k++, j++;
-
-		else if (i > maxL && j <= maxR)
-			array[k] = buff[j], k++, j++;
-		else
-			array[k] = buff[i], k++, i++;
-}
-/**
- * printcheck - prints an array in a given range
- *
- * @array: array of data to be print
- * @r1: start of range
- * @r2: end of range
- *
- * Return: No Return
- */
-void printcheck(int *array, int r1, int r2)
-{
-	int i;
-
-	for (i = r1; i <= r2; i++)
-	{
-		if (i > r1)
-			printf(", ");
-		printf("%d", array[i]);
-	}
-	printf("\n");
-}
-/**
- * split - recursive function to split data into merge tree
- *
- * @array: array of data to be split
- * @buff: auxiliary array of data for merging
- * @min: min range of data in array
- * @max: max range of data in array
- * @size: size of total data
- *
- * Return: No Return
- */
-void split(int *array, int *buff, int min, int max, int size)
-{
-	int mid, tmax, minL, maxL, minR, maxR;
-
-	if ((max - min) <= 0)
-		return;
-
-	mid = (max + min + 1) / 2;
-	tmax = max;
-	max = mid - 1;
-
-	minL = min;
-	maxL = max;
-
-	split(array, buff, min, max, size);
-
-	min = mid;
-	max = tmax;
-
-	minR = min;
-	maxR = max;
-
-	split(array, buff, min, max, size);
-
-	printf("Merging...\n");
-	printf("[left]: ");
-
-	printcheck(array, minL, maxL);
+	printf("Merging...\n[left]: ");
+	print_array(subarray + lo, mid - lo);
 
 	printf("[right]: ");
+	print_array(subarray + mid, hi - mid);
 
-	printcheck(array, minR, maxR);
-	merge(array, buff, minL, maxL, minR, maxR);
-	copy(array, buff, size);
-
+	for (low = lo, midi = mid; low < mid && midi < hi; a++)
+	{
+		if (subarray[low] < subarray[midi])
+		{
+			buffer[a] = subarray[low++];
+		}
+		else
+		{
+			buffer[a] = subarray[midi++];
+		}
+	}
+	for (; low < mid; low++)
+	{
+		buffer[a++] = subarray[low];
+	}
+	for (; midi < hi; midi++)
+	{
+		buffer[a++] = subarray[midi];
+	}
+	for (low = lo, a = 0; low < hi; low++)
+	{
+		subarray[low] = buffer[a++];
+	}
 	printf("[Done]: ");
-	printcheck(array, minL, maxR);
+	print_array(subarray + lo, hi - lo);
 }
+
 /**
- * merge_sort - sorts an array of integers in ascending order
- * using the Merge sort algorithm
- *
- * @array: array of data to be sorted
- * @size: size of data
- *
- * Return: No Return
+ * merge_sort_call - calling the merge sort function.
+ *@subarray: subarray to be sorted.
+ *@buffer: buffer of array.
+ *@lo: low partition.
+ *@hi: high partition.
+ * Return: Always 0
+ */
+void merge_sort_call(int *subarray, int *buffer, size_t lo, size_t hi)
+{
+	size_t mid;
+
+	if (hi - lo > 1)
+	{
+		mid = lo + (hi - lo) / 2;
+		merge_sort_call(subarray, buffer, lo, mid);
+		merge_sort_call(subarray, buffer, mid, hi);
+		merge_sub(subarray, buffer, lo, mid, hi);
+	}
+}
+
+/**
+ * merge_sort - merge sort algorithm implementation
+ *@array: array to be sorted.
+ *@size: size of array to be sorted.
+ * Return: void.
  */
 void merge_sort(int *array, size_t size)
 {
-	int *buff;
+	int *buffer;
 
-	if (size < 2)
+	buffer = malloc(sizeof(int) * size);
+	if (buffer == NULL)
 		return;
 
-	buff = malloc(sizeof(int) * size);
-	if (buff == NULL)
-		return;
+	merge_sort_call(array, buffer, 0, size);
 
-	copy(array, buff, size);
-
-	split(array, buff, 0, size - 1, size);
-
-	free(buff);
+	free(buffer);
 }
